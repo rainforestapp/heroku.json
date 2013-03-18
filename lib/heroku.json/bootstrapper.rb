0a765fe28c4618e2c3065fe13fa42eb616eeb904
Russell Smith <russell.smith@ukd1.co.uk>
2013-03-17 18:47:47 -0700

    Namespace + move to heroku.json folder

---
+++ lib/heroku.json/bootstrapper.rb
@@ -0,0 +1,45 @@
+module HerokuJson
+  class Bootstrapper < Struct.new(:api, :app, :json)
+    include Heroku::Helpers
+
+    def bootstrap
+      # create_app_if_not_exists
+      add_addons
+      add_config_vars
+      # push_code
+      # run_custom_commands
+    end
+
+    private
+
+    def create_app_if_not_exists
+
+    end
+
+    def add_addons
+      json['addons'].each do |addon|
+        action "Installing addon #{addon}" do
+          begin
+            api.post_addon(app, addon)
+          rescue Heroku::API::Errors::RequestFailed => e
+            status("already installed")
+          end
+        end
+      end
+    end
+
+    def add_config_vars
+      action "Deploying environment configs" do
+        api.put_config_vars(app, json['env'])
+      end
+    end
+
+    def push_code
+
+    end
+
+    def run_custom_commands
+
+    end
+  end
+end
---
+++ lib/heroku.json/describer.rb
@@ -0,0 +1,40 @@
+module HerokuJson
+  class Describer < Struct.new(:api, :app)
+    include Heroku::Helpers
+
+    def describe
+      {
+          'addons' => app_addons,
+          'env' => app_environment_variables
+      }
+    end
+
+    private
+
+    def app_addons
+      action "Getting addons for #{app}" do
+        body_or_die(api.get_addons(app)).map { |h| h['name'] }
+      end
+    end
+
+    def app_environment_variables
+      env = {}
+
+      action "Getting environment variables #{app}" do
+        body_or_die(api.get_config_vars(app)).each do |key, value|
+          # Skip if it's blacklisted
+          next if Array(HerokuJson::ENV_BLACKLIST).map { |re| Regexp.new(re).match(key) }.compact.first
+
+          env[key] = value
+        end
+      end
+
+      env
+    end
+
+    def body_or_die res
+      error("API request to Heroku failed with status #{res.status}. Aborted.") unless res.status == 200
+      res.body
+    end
+  end
+end
