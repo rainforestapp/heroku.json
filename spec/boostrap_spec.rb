fd6ff30dd4f0e10798a5b6a797ee5479788746eb
Russell Smith <russell.smith@ukd1.co.uk>
2013-03-18 19:17:20 -0700

    Disable integration tests if there is no heroku app configured

--- spec/boostrap_spec.rb
+++ spec/boostrap_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
-describe "#bootstrap" do
-  let(:user) { ENV['HEROKU_USER'] }
-  let(:password) { ENV['HEROKU_PASSWORD'] }
-  let(:client) { Heroku::Client.new(user, password) }
-  let(:command) { Heroku::Command::Json.new }
+if ENV['HEROKU_APP']
 
-  it "bootstrap stuff" do
-    ENV.delete 'HEROKU_APP'
-    command.bootstrap
+  describe "#bootstrap" do
+    let(:user) { ENV['HEROKU_USER'] }
+    let(:password) { ENV['HEROKU_PASSWORD'] }
+    let(:client) { Heroku::Client.new(user, password) }
+    let(:command) { Heroku::Command::Json.new }
+
+    it "bootstrap stuff" do
+      ENV.delete 'HEROKU_APP'
+      command.bootstrap
+    end
   end
+
 end
